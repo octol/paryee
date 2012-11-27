1e508978dda596538dfe05fd9e7c3944c11dd97e
Jon Haggblad <jon@haeggblad.com>
2012-11-27 08:44:07 +0000

    Completed performance tests

--- tests_perf.plt
+++ tests_perf.plt
@@ -1,3 +1,14 @@
-set terminal png truecolor
+set terminal png truecolor font "Helvetica"
 set out "tests_perf.png"
-plot "tests_perf.tsv" with lines
+set key bottom right
+set logscale xy
+set xlabel "Cells"
+set ylabel "Time [s]"
+plot \
+    "tests_perf.tsv" using 1:3 title "Ref" with linespoints, \
+    "tests_perf.tsv" using 1:2 title "Single thread" with linespoints, \
+    "tests_perf.tsv" using 1:4 title "OpenMP" with linespoints, \
+    "tests_perf.tsv" using 1:6 title "Pthread" with linespoints, \
+    "tests_perf.tsv" using 1:7 title "MPI" with linespoints
+
+    #"tests_perf.tsv" using 1:5 title "Pthread (naive)" with linespoints, \
--- tests_perf.sh
+++ tests_perf.sh
@@ -2,28 +2,27 @@
 # Run performance tests to compare the different implementations
 
 NODES=2
-NN="256 512 1024 2048 4096 8162"
+NN="1024 2048 4096 8192"
+NNN=1024
+#NN="1024 2048 4096 8192 16384 32768"
+#NN="8192 16384 32768 65536"
 OUTFILE=tests_perf.tsv
 
 MPI_NODES=$((NODES + 1))
 
 # Sanity checks
-if [ -f $OUTFILE ]
-then
-    echo "Output file already exists!"
-    exit 1;
-fi
+[ -f $OUTFILE ] && echo "$OUTFILE file already exists!" && exit 1;
 
-# Run tests
+echo "Performance tests:"
 for N in $NN
 do
     echo "Running for $N"
-    S1=$(./yee -n $N                         | grep 'Elapsed' | awk '{print $2}')
-    S2=$(./yee_ref -n $N                     | grep 'Elapsed' | awk '{print $2}')
-    S3=$(./yee_omp -n $N -t $NODES           | grep 'Elapsed' | awk '{print $2}')
-    S4=$(./yee_pthr -n $N -t $NODES          | grep 'Elapsed' | awk '{print $2}')
-    S5=$(./yee_pthr_barrier -n $N -t $NODES  | grep 'Elapsed' | awk '{print $2}')
-    S6=$(./yee_mpi -n $N -t $MPI_NODES       | grep 'Elapsed' | awk '{print $2}')
+    S1=$(./yee -n $N                          | grep 'Elapsed' | awk '{print $2}')
+    S2=$(./yee_ref -n $N                      | grep 'Elapsed' | awk '{print $2}')
+    S3=$(./yee_omp -n $N -t $NODES            | grep 'Elapsed' | awk '{print $2}')
+    S4=$(./yee_pthr -n $N -t $NODES           | grep 'Elapsed' | awk '{print $2}')
+    S5=$(./yee_pthr_barrier -n $N -t $NODES   | grep 'Elapsed' | awk '{print $2}')
+    S6=$(mpirun -n $MPI_NODES ./yee_mpi -n $N | grep 'Elapsed' | awk '{print $2}')
     echo $N $S1 $S2 $S3 $S4 $S5 $S6 >> $OUTFILE
 done
 
